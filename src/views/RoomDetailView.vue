ada9f4d926aa359260a2d7cca9a0a8303b1cbd51
tranduyviet <duyviet2841998@gmail.com>
2022-10-31 00:11:45 +0700

    update

--- src/views/RoomDetailView.vue
+++ src/views/RoomDetailView.vue
@@ -1,15 +1,18 @@
 <template>
   <dynamic-layout>
     <el-table :data="tableData" style="width: 100%" max-height="250">
-      <el-table-column fixed prop="date" label="Date" width="150" />
-      <el-table-column prop="name" label="Name" width="120" />
-      <el-table-column prop="state" label="State" width="120" />
-      <el-table-column prop="city" label="City" width="120" />
-      <el-table-column prop="address" label="Address" width="600" />
-      <el-table-column fixed="right" label="Operations" width="120">
+      <el-table-column prop="name" label="Name" width="200" />
+      <el-table-column prop="call" label="Call" width="200">
+        <el-input-number :min="1" :max="10" />
+      </el-table-column>
+      <el-table-column prop="all" label="All" width="200">
+        <el-input-number :min="1" :max="10" />
+      </el-table-column>
+      <el-table-column prop="total" label="Total" width="200" />
+      <el-table-column label="Operator" width="120">
         <template>
-          <ElButton size="small">Edit</ElButton>
-          <ElButton size="small" type="danger"> Delete </ElButton>
+          <el-button size="small">Edit</el-button>
+          <el-button size="small" type="danger"> Delete </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -31,22 +34,18 @@ import Room from "@/ui/Room.vue";
     tableData() {
       return [
         {
-          date: "2016-05-03",
+          id: "2016-05-03",
           name: "Tom",
-          state: "California",
-          city: "Los Angeles",
-          address: "No. 189, Grove St, Los Angeles",
-          zip: "CA 90036",
-          tag: "Home",
+          call: 1000,
+          all: 3200,
+          total: 1000,
         },
         {
-          date: "2016-05-03",
+          id: "2016-05-03",
           name: "Tom",
-          state: "California",
-          city: "Los Angeles",
-          address: "No. 189, Grove St, Los Angeles",
-          zip: "CA 90036",
-          tag: "Home",
+          call: 1000,
+          all: 3200,
+          total: -100,
         },
       ];
     },
