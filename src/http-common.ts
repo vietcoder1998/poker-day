707292214016861c274d4d60d4907c4f9a65701b
viettd <chaubv@admins-Mac-mini.local>
2022-11-08 14:00:47 +0700

    update

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,11 +1,12 @@
 /* eslint-disable */
 import axios from "axios";
 
-export const baseApi = `http://localhost:${process.env.API_PORT}/`;
+export const baseApi =  `http://174.129.71.31/api`;
 export const http = (headers?: any, api?: string) =>
   axios.create({
-    baseURL: api ?? `http://localhost:${process.env.API_PORT}/`,
+    baseURL: api ?? `http://localhost:${process.env.API_PORT}/api`,
     headers: { ...headers, "Content-Type": "application/json" } ?? {
       "Content-Type": "application/json"
     }
   });
+4
